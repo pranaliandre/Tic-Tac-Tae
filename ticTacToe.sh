3721e454ca8eedb8a24d8b73c99fc00b081a9efe
pranaliandre <pranaliandre06@gmail.com>
2020-03-26 13:33:48 -0500

    Added: Start fresh with resetting the board

--- ticTacToe.sh
+++ ticTacToe.sh
@@ -1,1 +1,10 @@
+#!/bin/bash -x
 echo "Welcome to tic tac toe game"
+
+#declare the gameboard
+declare boardGame
+#function of resetting the board
+function resetBoard(){
+	boardGame=(- - - - - - - - -)
+}
+resetBoard
